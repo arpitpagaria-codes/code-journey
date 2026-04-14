89280a772767b45ac8c6a816f238422be8f12177
arpitpagaria-codes <arpitpagaria7@gmail.com>
2026-04-14 11:30:46 +0530

    1800. Maximum Ascending Subarray Sum

---
+++ java/core-java/src/main/java/com/codejourney/core/MaximumAscendingSubarraySum_1800.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package com.codejourney.core;
+
+/**
+ * 1800. Maximum Ascending Subarray Sum Easy Topics premium lock icon Companies
+ * Hint Given an array of positive integers nums, return the maximum possible
+ * sum of an strictly increasing subarray in nums.
+ * 
+ * A subarray is defined as a contiguous sequence of numbers in an array.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [10,20,30,5,10,50] Output: 65 Explanation: [5,10,50] is the
+ * ascending subarray with the maximum sum of 65. Example 2:
+ * 
+ * Input: nums = [10,20,30,40,50] Output: 150 Explanation: [10,20,30,40,50] is
+ * the ascending subarray with the maximum sum of 150. Example 3:
+ * 
+ * Input: nums = [12,17,15,13,10,11,12] Output: 33 Explanation: [10,11,12] is
+ * the ascending subarray with the maximum sum of 33.
+ * 
+ * 
+ * Constraints:
+ * 
+ * 1 <= nums.length <= 100 1 <= nums[i] <= 100
+ */
+public class MaximumAscendingSubarraySum_1800 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] nums = new int[] {10,20,30,40,50};
+		System.out.println(maxAscendingSum(nums));
+
+	}
+
+	public static int maxAscendingSum(int[] nums) {
+		int maxSum = 0;
+		int currentSum = 0;
+		int previousNo = 0;
+		for (int a : nums) {
+			if (previousNo < a) {
+				currentSum += a;
+			} else {
+				currentSum = a;
+			}
+			if(maxSum < currentSum) {
+				maxSum = currentSum;
+			}
+			previousNo = a;
+
+		}
+		return maxSum;
+	}
+	
+	public static int maxAscendingSumV2(int[] nums) {
+	    int maxSum = nums[0];
+	    int currentSum = nums[0];
+
+	    for (int i = 1; i < nums.length; i++) {
+	        if (nums[i] > nums[i - 1]) {
+	            currentSum += nums[i];
+	        } else {
+	            currentSum = nums[i];
+	        }
+	        maxSum = Math.max(maxSum, currentSum);
+	    }
+
+	    return maxSum;
+	}
+
+}
